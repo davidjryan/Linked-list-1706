29dc73a9e9ff81e7730c3410391bde23264b6069
Spencer Harris <morrisonleary@gmail.com>
2017-07-10 13:13:31 -0600

    added script to javaScript

---
+++ script.js
@@ -0,0 +1,13 @@
+var readButton = document.querySelector(".read");
+var deleteButton = document.querySelector(".delete");
+
+
+readButton.addEventListener("click", function(){
+  console.log("read button clicked");
+
+});
+
+deleteButton.addEventListener("click", function(){
+  console.log("delete button clicked");
+
+});
