1f5769aff317a5f936c7ef8463ee2391a2f64e1f
David Ryan <djrdev@icloud.com>
2017-07-11 12:16:18 -0600

    jquery revision

--- script.js
+++ script.js
@@ -1,13 +1,69 @@
-var readButton = document.querySelector(".read");
-var deleteButton = document.querySelector(".delete");
+
+$('.enter').on('click', prependCard)
+$('.read').on('click', readButton)
+$('.delete').on('click', deleteButton)
+
+$(document).ready()
+
+function enterPressed() {
+  var titleInput = $('.title').val()
+  var urlInput = $('.url').val()
+
+  prependCard(titleInput, urlInput)
 
 
-readButton.addEventListener("click", function(){
-  console.log("read button clicked");
 
-});
+}
 
-deleteButton.addEventListener("click", function(){
-  console.log("delete button clicked");
+function prependCard(title, url) {
+  $('.main').prepend(`<article class="card">
+          <h2 class="title">${title}</h2>
+          <hr />
+          <a class="urllink" target="_blank" href="${url}">${url}</a>
+          <hr />
+          <section class="read-delete-container">
+            <button class="read">Read</button>
+            <button class="delete">Delete</button>
+          </section>
+        </article>`)
+}
 
-});
+function readButton() {
+
+}
+
+function deleteButton() {
+
+}
+
+function clearInput() {
+
+}
+
+function missingInput() {
+
+}
+
+function markAsRead() {
+
+}
+
+function enterDisabled() {
+
+}
+
+function cardTotal() {
+
+}
+
+function readUnread() {
+
+}
+
+function clearRead() {
+
+}
+
+function isUrl() {
+
+}
