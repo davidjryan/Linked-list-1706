5bc9c593fff67f6ded6c164a99666f8a6ca44761
Spencer Harris <morrisonleary@gmail.com>
2017-07-11 16:52:27 -0600

    added eventlistern for read, delete, enter. Knocked out phase 1-2, partial phase 3.

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
+var recordCount = 0;
 
-$('.enter').on('click', prependCard);
-$('.read-button').on('click', readButton);
-$('.delete-button').on('click', deleteButton);
+$('input').on('keyup', enterDisabled);
+$('.enter').on('click', enterPressed);
+$('main').on('click', '.read-button', readButton);
+$('main').on('click', '.delete-button', deleteButton);
 
 $( document ).ready(function() {
     console.log( "ready!" );
-    // $('.enter').prop('disabled', true);
+    $('.enter').prop('disabled', true);
 });
+
+
 
 function enterPressed() {
   var titleInput = $('.title').val();
   var urlInput = $('.url').val();
-  prependCard(titleInput, urlInput);
+  if(titleInput === "") {
+    alert("You are missing the title!");
+  } else if (urlInput === ""){
+    alert("You are missing the web url!");
+  } else if (!isUrl(urlInput)) {
+    alert("Your url is invalid!");
+  } else {
+    recordCount++;
+    prependCard(titleInput, urlInput);
+    clearInput();
+  }
 }
 
 function prependCard(title, url) {
@@ -28,20 +42,17 @@ function prependCard(title, url) {
 }
 
 function readButton() {
-  console.log("read button clicked");
+  $(this).closest('article').toggleClass('read');
 }
 
 function deleteButton() {
-  console.log("delete button clicked");
-$(this).closest('article').remove();
+  $(this).closest('article').remove();
 }
 
 function clearInput() {
-
-}
-
-function missingInput() {
-
+  $('.title').val('');
+  $('.url').val('');
+  $('.enter').prop('disabled', true);
 }
 
 function markAsRead() {
@@ -49,11 +60,18 @@ function markAsRead() {
 }
 
 function enterDisabled() {
-
+  if($('.title').val() !== '' &&
+    $('.url').val() !== '') {
+      $('.enter').prop('disabled', false);
+  } else {
+    console.log("failed enter");
+    $('.enter').prop('disabled', true);
+  }
 }
 
 function cardTotal() {
-
+  var articleList = document.querySelectorAll('article');
+  console.log(articleList);
 }
 
 function readUnread() {
@@ -64,6 +82,8 @@ function clearRead() {
 
 }
 
-function isUrl() {
-
+function isUrl(url) {
+  var regexQuery = "^(www\\.)?([-a-z0-9]{1,63}\\.)*?[a-z0-9][-a-z0-9]{0,61}[a-z0-9]\\.[a-z]{2,6}(/[-\\w@\\+\\.~#\\?&/=%]*)?$";
+ var pattern = new RegExp(regexQuery,"i");
+ return pattern.test(url);
 }
