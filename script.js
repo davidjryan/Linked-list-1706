62701f7d14e7b666d603c678b237ed5bdbfe5016
Spencer Harris <morrisonleary@gmail.com>
2017-07-11 13:04:20 -0600

    disabled enter button added form to index

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 
-$('.enter').on('click', prependCard)
-$('.read').on('click', readButton)
-$('.delete').on('click', deleteButton)
+$('.enter').on('click', prependCard);
+$('.read-button').on('click', readButton);
+$('.delete-button').on('click', deleteButton);
 
-$(document).ready()
+$( document ).ready(function() {
+    console.log( "ready!" );
+    $('.enter').prop('disabled', true);
+});
 
 function enterPressed() {
-  var titleInput = $('.title').val()
-  var urlInput = $('.url').val()
-
-  prependCard(titleInput, urlInput)
-
-
-
+  var titleInput = $('.title').val();
+  var urlInput = $('.url').val();
+  prependCard(titleInput, urlInput);
 }
 
 function prependCard(title, url) {
-  $('.main').prepend(`<article class="card">
+  $('main').prepend(`<article class="card">
           <h2 class="title">${title}</h2>
           <hr />
           <a class="urllink" target="_blank" href="${url}">${url}</a>
@@ -25,15 +24,16 @@ function prependCard(title, url) {
             <button class="read">Read</button>
             <button class="delete">Delete</button>
           </section>
-        </article>`)
+        </article>`);
 }
 
 function readButton() {
-
+  console.log("read button clicked");
 }
 
 function deleteButton() {
-
+  console.log("delete button clicked");
+$(this).closest('article').remove();
 }
 
 function clearInput() {
